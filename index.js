5585a1acfefc65aac499e7713ab8b6ae10191179
AlejandroBR10 <alejabaldera@gmail.com>
2024-09-21 18:19:02 -0600

    Commit inicial

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require("express");
 const app = express();
-const { pokemon } = require("./pokedex.json");
+const pokemon = require('./routes/pokemon');
 
-
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 /*
 Verbos HTTP
 GET - obtener recursos
-POST - almacenar/crear recursos
+POST - almacenar/crear recursos   
 PUT - modificar un recurso completo
 PATCH - modificar una parte recurso
 DELETE - borrar un recurso
@@ -19,32 +21,7 @@ DELETE - borrar un recurso
 app.get("/", (req, res, next) => {
   return res.status(200).send("Bienvenido al pokedex");
 });
-
-app.post("/pokemon",(req,res,next) => {
-    return res.status(200).send(req.body);
-});
-
-app.get("/pokemon", (req, res, next) => {
-  return res.status(200).send(pokemon);
-});
-app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
-  const id = req.params.id - 1;
-  (id >= 0 && id <= 150) ? 
-    res.status(200).send(pokemon[req.params.id - 1]) : 
-    res.status(404).send("Pokemon no encontrado");
-
-});
-app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
-  const name = req.params.name;
-  const pk = pokemon.filter((p) => {
-    return (p.name.toUpperCase() == name.toUpperCase()) && p ;
-  });
-
-  (pk.length > 0) ? 
-      res.status(200).send(pk) : 
-      res.status(404).send("Pokemon no encontrado");
-  
-});
+app.use("/pokemon",pokemon );
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running...");
